308cc53d349f4b0a3a348b15867eec06db68e3e0
carma <cmarti@campuscamarafp.com>
2021-02-11 21:32:49 +0100

    interfaz acabada

--- Generadorcontraseñas/src/primerpanel.java
+++ Generadorcontraseñas/src/primerpanel.java
@@ -2,6 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+import static java.awt.Font.*;
 
 public class primerpanel extends JFrame {
 
@@ -11,74 +15,145 @@ public class primerpanel extends JFrame {
  private JSpinner spinner;
  private JCheckBox minuscula,mayuscula,numeros,simbolos;
  private SpinnerModel spin= new SpinnerNumberModel(1,1,20,1);
+ private JProgressBar barra;
+ private JButton generar;
+ private JButton cancelar;
+ Font f = new Font("Agency FB", BOLD, 20);
+ Font g = new Font("Thaoma", BOLD + ITALIC, 15);
+ Font h = new Font("Times New Roman", BOLD, 14);
 
  public primerpanel(){
      super("Esto es el generador de contraseñas");
      setDefaultCloseOperation(EXIT_ON_CLOSE);
      setLayout(new GridLayout(3,1));
-     JPanel p1= new JPanel();
-     JPanel p2= new JPanel();
+     JPanel general= new JPanel();
+     JPanel primero= new JPanel();
 
-    JPanel p3 = new JPanel();
-    p3.setLayout(new FlowLayout());
+     JPanel texto = new JPanel();
+     texto.setLayout(new FlowLayout());
      textcontra= new JTextField("");
      textcontra.setBackground(new Color(252, 240, 131));
      copiar=new JButton("Copiar");
-     copiar.setBackground(new Color(220, 145, 230));
+     copiar.setBackground(new Color(122, 222, 255));
      copiar.setBounds(new Rectangle(20,20,50,20));
      copiar.setSize(30,30);
-     p3.add(copiar);
+     copiar.setFont(h);
+     texto.add(copiar);
      h1 = new JLabel("Personalice la contraseña requerida");
-     p2.setLayout(new GridLayout(3,1));
-     p2.add(textcontra);
-     p2.add(p3);
+     h1.setFont(f);
+     primero.setLayout(new GridLayout(3,1));
+     primero.add(textcontra);
+     primero.add(texto);
      //p2.add(copiar);
-     p2.add(h1);
+     primero.add(h1);
      copiar.addActionListener(new Listenercopiar1());
 
 
-     JPanel p4 = new JPanel();
-     p4.setLayout(new GridLayout(1,2,20,20));
-     JPanel p5 =new JPanel();
-     p5.setLayout(new GridLayout(3,1));
+     JPanel segundo = new JPanel();
+     segundo.setLayout(new GridLayout(1,2,20,20));
+     JPanel segundo2 =new JPanel();
+     segundo2.setLayout(new GridLayout(3,1));
      JLabel vacio1=new JLabel("");
-     p5.add(vacio1);
-     JPanel p7=new JPanel();
+     segundo2.add(vacio1);
+     JPanel spinn=new JPanel();
         spinner = new JSpinner(spin);
         spinner.setPreferredSize(new Dimension(100,30));
-        p7.add(spinner);
-        p5.add(p7);
+        spinn.add(spinner);
+        segundo2.add(spinn);
         JLabel vacio2= new JLabel("");
-        p5.add(vacio2);
-    JPanel p6 = new JPanel();
+        segundo2.add(vacio2);
+        JPanel lista = new JPanel();
 
-        minuscula=new JCheckBox("Minuscula");
+     minuscula=new JCheckBox("Minuscula");
+     minuscula.setFont(g);
      mayuscula=new JCheckBox("Mayuscula");
+     mayuscula.setFont(g);
      numeros = new JCheckBox("Numeros");
+     numeros.setFont(g);
      simbolos=new JCheckBox("Simbolos");
-     p6.setLayout(new GridLayout(4,1));
-     p6.add(minuscula);
-     p6.add(mayuscula);
-     p6.add(numeros);
-     p6.add(simbolos);
+     simbolos.setFont(g);
+     lista.setLayout(new GridLayout(4,1));
+     lista.add(minuscula);
+     lista.add(mayuscula);
+     lista.add(numeros);
+     lista.add(simbolos);
 
-     p4.add(p5);
-    p4.add(p6);
+     segundo.add(segundo2);
+     segundo.add(lista);
+
+     JPanel tercero = new JPanel();
+     tercero.setLayout(new GridLayout(3,1));
+
+     JPanel barrap = new JPanel();
+     barra= new JProgressBar();
+     barra.getValue();
+     barrap.add(barra);
+     JPanel botones = new JPanel();
+     botones.setLayout(new GridLayout(1,3,30,30));
+     JLabel vacio3= new JLabel("");
+     botones.add(vacio3);
 
 
-     p1.add(p4);
-     p1.add(p2);
+     cancelar=new JButton("Cancelar");
+     cancelar.setBackground(new Color(232, 91, 74));
+     cancelar.setFont(h);
+     botones.add(cancelar);
+     generar=new JButton("Generar");
+     generar.setBackground(new Color(64, 199, 102));
+     general.setFont(h);
+     //generar.setSize(60,60);
+     botones.add(generar);
+     tercero.add(barrap);
+     tercero.add(botones);
+     cancelar.addActionListener(new Listenercopiar2());
+     generar.addActionListener(new Listenercopiar3());
 
-     add(p1,BorderLayout.NORTH);
-     add (p4,BorderLayout.CENTER);
+
+
+     general.add(tercero);
+     general.add(segundo);
+     general.add(primero);
+
+     add(general,BorderLayout.NORTH);
+     add (segundo,BorderLayout.CENTER);
+     add(tercero,BorderLayout.SOUTH);
      setSize(400,400);
      setVisible(true);
  }
+
+    private String getSansSerif() {
+        return SANS_SERIF;
+    }
 
     public static void main(String[] args) {
         primerpanel pp = new primerpanel();
     }
     private class Listenercopiar1 implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                FileWriter documento = new FileWriter  ("C:\\Users\\carma\\IdeaProjects\\entregable5\\Generadorcontraseñas\\src\\contraseñas", true);
+                PrintWriter pw = new PrintWriter (documento);
+                pw.println(contraseña[resultado]);
+                pw.close();
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+
+
+        }
+
+        }
+    }
+
+    private class Listenercopiar2 implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private class Listenercopiar3 implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
 
